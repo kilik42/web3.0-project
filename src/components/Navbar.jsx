152d7f360b6d2c8d3c1d61d112bbae269e2886d6
kilik42 <marvinevins@yahoo.com>
2022-03-29 06:01:00 -0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {HiMenuAlt4} from 'react-icons/hi'
+import {AiOutlineClose} from 'react-icons/ai'
+import logo from '../../images/logo.png'
+
+const NavbarItem = ({title, classprops}) => {
+  return(
+  
+  <li className={`mx-4 cursor-pointer ${classprops}`}>
+    {title}
+  </li>  )
+}
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <img src={logo} alt="" className="w-32 cursor-pointer"/>
+
+      </div>
+      <ul className="text-white md:flex hiddent list-none flex row justify-between items-center flex-initial">
+        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+            <NavbarItem key={item +index} title={item}/>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
